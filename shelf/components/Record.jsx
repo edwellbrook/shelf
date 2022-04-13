0518a48365a984590d76b913d554cffe8a99fc34
Ed Wellbrook <edwellbrook@gmail.com>
2022-04-13 14:14:04 +0100

    Tidy code

--- shelf/components/Record.jsx
+++ shelf/components/Record.jsx
@@ -1,35 +1,44 @@
 import { useRef, useEffect } from "react";
 import VanillaTilt from "vanilla-tilt";
 
-function Tilt({ options, ...props }) {
+function Tilt({ options, children, ...props }) {
   const ref = useRef(null);
 
   useEffect(() => {
     VanillaTilt.init(ref.current, options);
   }, []);
 
-  return <div ref={ref} {...props} />;
+  return (
+    <div className="wrapper">
+      <div ref={ref} {...props}>
+        {children}
+
+        <div className="js-tilt-glare">
+          <div className="js-tilt-glare-inner" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default function Record({ title, artist, artworkURL, discogsURL }) {
   return (
     <li className="record">
       <a href={discogsURL}>
-        <div className="shadow">
-          <Tilt
-            className="wrapper"
-            options={{
-              scale: 1.05,
-              reverse: false,
-              glare: true,
-              "max-glare": 0.5,
-            }}
-          >
-            <div className="center">
-              <img className="art" src={artworkURL} alt="" />
-            </div>
-          </Tilt>
-        </div>
+        <Tilt
+          className="shadow"
+          options={{
+            scale: 1.05,
+            reverse: false,
+            glare: true,
+            "max-glare": 0.5,
+            "glare-prerender": true,
+          }}
+        >
+          <div className="center">
+            <img className="art" src={artworkURL} alt="" />
+          </div>
+        </Tilt>
 
         <div className="metadata">
           <p className="title">{title}</p>
