b574c9a9217f2dc43fadb8e012bb9e396975999c
Ed Wellbrook <edwellbrook@gmail.com>
2022-06-03 16:57:07 +0100

    Use featured for homepage list

--- pages/index.js
+++ pages/index.js
@@ -10,50 +10,68 @@ import Footer from "@/components/Footer"
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 function useAccount(accountToken) {
-  const url = `https://musicthread.app/api/v0/account/${accountToken}`
-  const { data, error } = useSWR(url, fetcher)
+    const url = `https://musicthread.app/api/v0/account/${accountToken}`
+    const { data, error } = useSWR(url, fetcher)
 
-  return {
-    account: data?.account,
-    threads: data?.threads,
-    isLoading: !error && !data,
-    isError: error,
-  }
+    return {
+        account: data?.account,
+        threads: data?.threads,
+        isLoading: !error && !data,
+        isError: error,
+    }
+}
+
+function useFeatured() {
+    const url = `https://musicthread.app/api/v0/featured`
+    const { data, error } = useSWR(url, fetcher)
+
+    return {
+        threads: data?.threads,
+        isLoading: !error && !data,
+        isError: error,
+    }
 }
 
 export default function Home() {
-  const accountToken = "8mtCK1RdXh4SopcY0-9Ywn5I0HBynZq-hHO9ig0pObI"
-  const { account, threads, isLoading, isError } = useAccount(accountToken)
+    // const accountToken = "8mtCK1RdXh4SopcY0-9Ywn5I0HBynZq-hHO9ig0pObI"
+    // const { account, threads, isLoading, isError } = useAccount(accountToken)
+    const { threads, isLoading, isError } = useFeatured()
 
-  if (isLoading) {
-    return <span>Loading...</span>
-  }
+    if (isLoading) {
+        return <span>Loading...</span>
+    }
 
-  if (isError) {
-    return <span>Error...</span>
-  }
+    if (isError) {
+        return <span>Error...</span>
+    }
 
-  return (
-    <div id="wrap">
-      <Head>
-        <title>Shelf</title>
+    return (
+        <div id="wrap">
+            <Head>
+                <title>Shelf</title>
 
-        <Script src="/vanilla-tilt.js" />
-      </Head>
+                <Script src="/vanilla-tilt.js" />
+            </Head>
 
-      <main class="content clearfix">
-        <ul>
-          {threads.map((thread) => (
-            <li key={thread.key}>
-              <Link href={`/thread/${thread.key}`}>
-                <a>{thread.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
+            <main class="content clearfix">
+                <ul>
+                    {threads.map((thread) => (
+                        <li key={thread.key}>
+                            <Link href={`/thread/${thread.key}`}>
+                                <a>
+                                    <div>
+                                        {thread.title}
+                                        <br />
+                                        by {thread.author.name}
+                                    </div>
+                                </a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </main>
 
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
