334a36f45d7c3aecc8c2f7d6105ec4e0b462827a
Ed Wellbrook <edwellbrook@gmail.com>
2022-06-03 17:03:04 +0100

    Updates

--- components/RecordGrid.jsx
+++ components/RecordGrid.jsx
@@ -1,71 +1,26 @@
-import useSWR from "swr"
-
 import Record from "./Record"
 
-function RecordGridLoading({}) {
-  return (
-    <transition-group name="zoom">
-      <div id="loading-spinner" key="theSpinner">
-        <div className="progress-text">
-          <span>Loading...</span>
-          <div className="spinner"></div>
-        </div>
-      </div>
-    </transition-group>
-  )
-}
+export default function RecordGrid({ thread, links, size }) {
+    const sizeOptions = {
+        l: "large",
+        xl: "extra_large",
+    }
+    const sizeClass = sizeOptions[size]
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+    return (
+        <section id="records">
+            <header>
+                <h1 id="thread_title">{thread.title}</h1>
+                <span>by {thread.author.name}</span>
 
-function useThread(threadKey) {
-  const url = `https://musicthread.app/api/v0/thread/${threadKey}`
-  const { data, error } = useSWR(url, fetcher)
+                {thread.description ? <p className="description">{thread.description}</p> : null}
+            </header>
 
-  return {
-    thread: data?.thread,
-    links: data?.links,
-    isLoading: !error && !data,
-    isError: error,
-  }
-}
-
-export default function RecordGrid({ threadId, size }) {
-  const { thread, links, isLoading, isError } = useThread(threadId)
-
-  if (isLoading) {
-    return <RecordGridLoading />
-  }
-
-  if (isError) {
-    return <span>Error</span>
-  }
-
-  const sizeOptions = {
-    l: "large",
-    xl: "extra_large",
-  }
-  const sizeClass = sizeOptions[size]
-
-  return (
-    <section id="records">
-      <header>
-        <h1 id="thread_title">{thread.title}</h1>
-        <span>by {thread.author.name}</span>
-
-        {thread.description ? <p className="description">{thread.description}</p> : null}
-      </header>
-
-      <ul className={sizeClass}>
-        {links.map((link) => (
-          <Record
-            key={link.key}
-            title={link.title}
-            artist={link.artist}
-            artworkURL={link.thumbnail_url}
-            discogsURL={link.page_url}
-          />
-        ))}
-      </ul>
-    </section>
-  )
+            <ul className={sizeClass}>
+                {links.map((link) => (
+                    <Record key={link.key} title={link.title} artist={link.artist} artworkURL={link.thumbnail_url} discogsURL={link.page_url} />
+                ))}
+            </ul>
+        </section>
+    )
 }
--- pages/thread/[thread_id].js
+++ pages/thread/[thread_id].js
@@ -2,31 +2,93 @@ import Head from "next/head"
 import Image from "next/image"
 import Script from "next/script"
 import { useRouter } from "next/router"
+import useSWR from "swr"
 
 import RecordGrid from "@/components/RecordGrid"
 import Footer from "@/components/Footer"
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+function useThread(threadKey) {
+    const url = `https://musicthread.app/api/v0/thread/${threadKey}`
+    const { data, error } = useSWR(url, fetcher)
+
+    return {
+        thread: data?.thread,
+        links: data?.links,
+        isLoading: !error && !data,
+        isError: error,
+    }
+}
+
+function RecordGridLoading({}) {
+    return (
+        <transition-group name="zoom">
+            <div id="loading-spinner" key="theSpinner">
+                <div className="progress-text">
+                    <span>Loading...</span>
+                    <div className="spinner"></div>
+                </div>
+            </div>
+        </transition-group>
+    )
+}
+
 export default function ThreadPage({ threadId }) {
-  const router = useRouter()
+    const router = useRouter()
 
-  return (
-    <div id="wrap">
-      <Head>
-        <title>Shelf</title>
-        <link rel="canonical" href={`https://musicthread.app/thread/${threadId}`} />
+    const { thread, links, isLoading, isError } = useThread(threadId)
 
-        <Script src="/vanilla-tilt.js" />
-      </Head>
+    if (isLoading) {
+        return (
+            <div id="wrap">
+                <Head>
+                    <title>Shelf</title>
+                    <link rel="canonical" href={`https://musicthread.app/thread/${threadId}`} />
+                </Head>
 
-      <main className="content clearfix">
-        <RecordGrid threadId={threadId} size={router.query.size} />
-      </main>
+                <main className="content clearfix">
+                    <RecordGridLoading />
+                </main>
 
-      <Footer />
-    </div>
-  )
+                <Footer />
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div id="wrap">
+                <Head>
+                    <title>Shelf</title>
+                    <link rel="canonical" href={`https://musicthread.app/thread/${threadId}`} />
+                </Head>
+
+                <main className="content clearfix">
+                    <span>Error loading page</span>
+                </main>
+
+                <Footer />
+            </div>
+        )
+    }
+
+    return (
+        <div id="wrap">
+            <Head>
+                <title>{thread.title} &bulll; Shelf</title>
+                <link rel="canonical" href={`https://musicthread.app/thread/${threadId}`} />
+            </Head>
+
+            <main className="content clearfix">
+                <RecordGrid thread={thread} links={links} size={router.query.size} />
+            </main>
+
+            <Footer />
+        </div>
+    )
 }
 
 ThreadPage.getInitialProps = (ctx) => {
-  return { threadId: ctx.query.thread_id }
+    return { threadId: ctx.query.thread_id }
 }
